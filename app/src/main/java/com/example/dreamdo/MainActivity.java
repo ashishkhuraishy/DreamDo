8ac3ecb9ce244a1d1d03c71e34557ce80980c5ff
ashishkhuraishY <ashishkhuraishy@gmail.com>
2019-09-24 20:32:01 +0530

    Added Fragment

--- app/src/main/java/com/example/dreamdo/MainActivity.java
+++ app/src/main/java/com/example/dreamdo/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.dreamdo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements toDo.FragmentListener {
+
+    private toDo toDoFragment;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text1);
+
+        toDoFragment = new toDo();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container1, toDoFragment)
+                .commit();
+
+
+    }
+
+    @Override
+    public void OnInputInterface(CharSequence input) {
+
+        text.setText(input);
     }
 }
